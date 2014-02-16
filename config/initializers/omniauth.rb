6cc27f4b82c795587298fbfd75ba1323b5cc520d
Brian Yingling <brian@daft.io>
2014-02-16 18:36:15 -0500

    of course now google drive api works

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,10 @@ OmniAuth.config.full_host = "http://localhost:3000"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_TODO_OAUTH_CLIENT_ID"], ENV["GOOGLE_API_TODO_SECRET_KEY"], 
   {
-    :scope => 'userinfo.profile, userinfo.email, drive',
+    :scope => 'userinfo.profile, userinfo.email, drive, https://docs.google.com/feeds/, https://docs.googleusercontent.com/, https://spreadsheets.google.com/feeds/',
     :provider_ignores_state => true
   }
 end
+
+
+
